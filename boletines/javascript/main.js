f9a12db1ad18fc5fcddc85748525a72033ca2c81
YonIvanMarquezBuitrago <yimbunal@hotmail.com>
2020-03-30 19:42:31 -0500

    Primera Entrega

---
+++ boletines/javascript/main.js
@@ -0,0 +1,38 @@
+//Obtener los elementos de la calse .close
+let links = document.querySelectorAll(".close");
+
+//Recorrerlos
+links.forEach(function(links){
+    //Agregar un evento click a cada uno de ellos
+    links.addEventListener("click",function(evento){
+        evento.preventDefault();
+       
+        let content = document.querySelector(".content");
+
+        //Quitarle las clases de animacion que ya tiene
+        content.classList.remove("fadeInDown");
+        content.classList.remove("animated");
+         
+        //Agregarle las clases para animar su salida 
+        content.classList.add("fadeOutUp");
+        content.classList.add("animated");
+
+        setTimeout(function(){
+            location.href="/";
+        },1000);
+        
+
+        return false;
+
+    })
+});
+
+/*
+//Obtener los elementos de la calse .close
+let iconos = document.querySelectorAll("i");
+//Recorrerlos
+iconos.forEach(function(iconos){
+    
+    iconos.classList.remove("far fa-star")
+});
+*/
